b6265e6ab20b0a7fd8f0a2d1fe2f116b18eb33f2
pierreosselin <pierre.osselin@gmail.com>
2020-01-17 16:48:56 +0100

    Initial NeuralUCB

---
+++ research/deep_contextual_bandits/bandits/algorithms/neuralUCB.py
@@ -0,0 +1,100 @@
+# Copyright 2018 The TensorFlow Authors All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Contextual bandit algorithm based on Thompson Sampling and a Bayesian NN."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from bandits.core.bandit_algorithm import BanditAlgorithm
+from bandits.algorithms.bb_alpha_divergence_model import BBAlphaDivergence
+from bandits.algorithms.bf_variational_neural_bandit_model import BfVariationalNeuralBanditModel
+from bandits.core.contextual_dataset import ContextualDataset
+from bandits.algorithms.multitask_gp import MultitaskGP
+from bandits.algorithms.neural_bandit_model import NeuralBanditModel
+from bandits.algorithms.variational_neural_bandit_model import VariationalNeuralBanditModel
+
+
+class NeuralUCBSampling(BanditAlgorithm):
+  """UCB Sampling algorithm based on a neural network."""
+
+  def __init__(self, name, hparams, bnn_model='RMSProp'):
+    """Creates a PosteriorBNNSampling object based on a specific optimizer.
+
+    The algorithm has two basic tools: an Approx BNN and a Contextual Dataset.
+    The Bayesian Network keeps the posterior based on the optimizer iterations.
+
+    Args:
+      name: Name of the algorithm.
+      hparams: Hyper-parameters of the algorithm.
+      bnn_model: Type of BNN. By default RMSProp (point estimate).
+    """
+
+    self.name = name
+    self.hparams = hparams
+    self.optimizer_n = hparams.optimizer
+
+    self.training_freq = hparams.training_freq
+    self.training_epochs = hparams.training_epochs
+    self.t = 0
+    self.gamma = 0
+    self.Zinv = (1/hparams.lambda) * np.eye(hparams.lambda)
+    self.bonus = np.zeros(hparams.num_actions)
+    self.C1 = 1
+    self.C2 = 1
+    self.C3 = 1
+    self.data_h = ContextualDataset(hparams.context_dim, hparams.num_actions,
+                                    hparams.buffer_s)
+
+    # to be extended with more BNNs (BB alpha-div, GPs, SGFS, constSGD...)
+    bnn_name = '{}-ucb'.format(name)
+    self.bnn = NeuralBanditModel(self.optimizer_n, hparams, bnn_name)
+
+  def action(self, context):
+    """Selects action for context based on UCB using the NN."""
+
+    if self.t < self.hparams.num_actions * self.hparams.initial_pulls:
+      # round robin until each action has been taken "initial_pulls" times
+      return self.t % self.hparams.num_actions
+
+    with self.bnn.graph.as_default():
+      c = context.reshape((1, self.hparams.context_dim))
+      output = self.bnn.sess.run(self.bnn.y_pred, feed_dict={self.bnn.x: c})
+
+      ### Add confidence bound to outbut
+      output += bonus
+      return np.argmax(output)
+
+  def update(self, context, action, reward):
+    """Updates data buffer, and re-trains the BNN every training_freq steps."""
+
+    self.t += 1
+    self.data_h.add(context, action, reward)
+
+    if self.t % self.training_freq == 0:
+      if self.hparams.reset_lr:
+        self.bnn.assign_lr()
+      self.bnn.train(self.data_h, self.training_epochs)
+
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(self.bnn.y_pred[action], tvars), self.hparams.max_grad_norm)
+    outer = np.outer(grads,grads) / self.hparams.layer_sizes[0]
+    self.Zinv -= self.Zinv.dot(outer.dot(self.Zinv))/(1 + grads.T.dot(self.Zinv.dot(grads)))
+    self.gamma = np.sqrt(1 + self.C1*((self.hparams.layer_sizes[0])**(-1/6))*np.sqrt(np.log(self.hparams.layer_sizes[0])) * (len(self.hparams.layer_sizes)**4) * (self.t**(7/6)) * (self.hparams.lambda ** (-7/6))  )
+    self.gamma *= self.hparams.mu * np.sqrt(-np.log(np.linalg.det(self.hparams.lambda * self.Zinv)) + self.C2 * ((self.hparams.layer_sizes[0])**(-1/6))*np.sqrt(np.log(self.hparams.layer_sizes[0])) * (len(self.hparams.layer_sizes)**4) * (self.t**(5/3)) * (self.hparams.lambda ** (-1/6)) - 2*np.log(self.hparams.delta)  ) + np.sqrt(self.hparams.lambda)*self.hparams.S
+    self.gamma += self.C3*((1 - self.hparams.mu * self.hparams.layer_sizes[0] * self.hparams.lambda )**(self.training_epochs) * np.sqrt(self.t/self.hparams.lambda) + ((self.hparams.layer_sizes[0])**(-1/6))*np.sqrt(np.log(self.hparams.layer_sizes[0])) * (len(self.hparams.layer_sizes)**(7/2)) * (self.t**(5/3)) * (self.hparams.lambda ** (-5/3)) * (1 + np.sqrt(self.t/self.hparams.lambda)))
--- research/deep_contextual_bandits/example_main.py
+++ research/deep_contextual_bandits/example_main.py
@@ -231,7 +231,7 @@ def main(_):
 
   # Problem parameters
   num_contexts = 2000
-  
+
   # parameters of finite
   tfn=400
   tfe=tfn*2
@@ -256,7 +256,7 @@ def main(_):
                                                b0=6,
                                                lambda_prior=0.25,
                                                initial_pulls=2)
- 
+
   hparams_rms = tf.contrib.training.HParams(num_actions=num_actions,
                                             context_dim=context_dim,
                                             init_scale=0.3,
@@ -364,7 +364,7 @@ def main(_):
                                                  a0=6,
                                                  b0=6,
                                                  lambda_prior=0.25)
-                                                 
+
   hparams_nlinear_finite_memory = tf.contrib.training.HParams(num_actions=num_actions,
                                                 context_dim=context_dim,
                                                 init_scale=0.3,
@@ -508,6 +508,29 @@ def main(_):
                                            optimizer='RMS',
                                            task_latent_dim=5,
                                            activate_decay=False)
+
+    hparams_ucb = tf.contrib.training.HParams(num_actions=num_actions,
+                                             num_outputs=num_actions,
+                                             context_dim=context_dim,
+                                             reset_lr=False,
+                                             learn_embeddings=True,
+                                             max_num_points=1000,
+                                             show_training=False,
+                                             freq_summary=1000,
+                                             batch_size=512,
+                                             keep_fixed_after_max_obs=True,
+                                             training_freq=50,
+                                             initial_pulls=2,
+                                             training_epochs=100,
+                                             lr=0.01,
+                                             buffer_s=-1,
+                                             initial_lr=0.001,
+                                             lr_decay_rate=0.0,
+                                             optimizer='RMS',
+                                             task_latent_dim=5,
+                                             activate_decay=False,
+                                             delta = 0.01,
+                                             lambda = 0.01)
 
   algos = [
       UniformSampling('Uniform Sampling', hparams),
